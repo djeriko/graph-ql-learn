06d9efde2f2d77c0a88f9056179e4fd210f3708d
djeriko <jerikosalivan@gmail.com>
2020-03-29 20:42:00 +0900

    Connected to MongoDB

--- schema/schema.js
+++ schema/schema.js
@@ -10,26 +10,50 @@ const {
 } = graphql
 
 const movies = [
-  { id: "1", name: "Pulp Fiction", genre: "Crime", directorId: "1" },
-  { id: "2", name: "1984", genre: "Sci-Fi", directorId: "2" },
-  { id: "3", name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "3" },
-  { id: "4", name: "Snatch", genre: "Crime-Comedy", directorId: "4" },
-  { id: "5", name: "Reservoir Dogs", genre: "Crime", directorId: "1" },
-  { id: "6", name: "The Hateful Eight", genre: "Crime", directorId: "1" },
-  { id: "7", name: "Inglourious Basterds", genre: "Crime", directorId: "1" },
+  { id: "1", name: "Pulp Fiction", genre: "Crime", directorId: "5e804a236be1261733294c4f" },
+  { id: "2", name: "1984", genre: "Sci-Fi", directorId: "5e804a236be1261733294c4f" },
+  { id: "3", name: "V for vendetta", genre: "Sci-Fi-Triller", directorId: "5e804a236be1261733294c51" },
+  { id: "4", name: "Snatch", genre: "Crime-Comedy", directorId: "5e804a236be1261733294c52" },
+  { id: "5", name: "Reservoir Dogs", genre: "Crime", directorId: "5e804a236be1261733294c4f" },
+  { id: "6", name: "The Hateful Eight", genre: "Crime", directorId: "5e804a236be1261733294c4f" },
+  { id: "7", name: "Inglourious Basterds", genre: "Crime", directorId: "5e804a236be1261733294c4f" },
   {
-    id: "7",
+    id: "8",
     name: "Lock, Stock and Two Smoking Barrels",
     genre: "Crime-Comedy",
-    directorId: "4"
+    directorId: "5e804a236be1261733294c52"
   }
 ]
+
+const moviesJSON = [
+  { "id": "1", "name": "Pulp Fiction", "genre": "Crime", "directorId": "5e804a236be1261733294c4f" },
+  { "id": "2", "name": "1984", "genre": "Sci-Fi", "directorId": "5e804a236be1261733294c4f" },
+  { "id": "3", "name": "V for vendetta", "genre": "Sci-Fi-Triller", "directorId": "5e804a236be1261733294c51" },
+  { "id": "4", "name": "Snatch", "genre": "Crime-Comedy", "directorId": "5e804a236be1261733294c52" },
+  { "id": "5", "name": "Reservoir Dogs", "genre": "Crime", "directorId": "5e804a236be1261733294c4f" },
+  { "id": "6", "name": "The Hateful Eight", "genre": "Crime", "directorId": "5e804a236be1261733294c4f" },
+  { "id": "7", "name": "Inglourious Basterds", "genre": "Crime", "directorId": "5e804a236be1261733294c4f" },
+  {
+    "id": "8",
+    "name": "Lock, Stock and Two Smoking Barrels",
+    "genre": "Crime-Comedy",
+    "directorId": "5e804a236be1261733294c52"
+  }
+]
+
 
 const directors = [
   { id: "1", name: "Quentin Tarantino", age: 55 },
   { id: "2", name: "Michael Radford", age: 72 },
   { id: "3", name: "James McTeigue", age: 51 },
   { id: "4", name: "Guy Ritchie", age: 50 }
+]
+
+const directorsJSON = [
+  { "id": "1", "name": "Quentin Tarantino", "age": 55 },
+  { "id": "2", "name": "Michael Radford", "age": 72 },
+  { "id": "3", "name": "James McTeigue", "age": 51 },
+  { "id": "4", "name": "Guy Ritchie", "age": 50 }
 ]
 
 const MovieType = new GraphQLObjectType({
